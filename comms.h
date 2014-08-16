17fa38877a7394d0fd055306e21ca67dca36589f
Fab <fab@ubuntu.(none)>
2014-08-16 16:58:24 +0200

    Adding channels for uart <-> sensor communication.

--- comms.h
+++ comms.h
@@ -8,12 +8,12 @@
 #define __COMMS_H__
 
 #define SENSORS_CHAN "ipc:///tmp/sensors"
-#define IMU_CHAN "ipc:///tmp/imu"
+#define IMU_GYRO_CHAN "ipc:///tmp/imu_gyro"
+#define IMU_MAG_CHAN "ipc:///tmp/imu_mag"
+#define IMU_ACCEL_CHAN "ipc:///tmp/imu_accel"
 #define GPS_CHAN "ipc:///tmp/gps"
-#define RC_CHAN "ipc:///tmp/rc"
 #define ACTUATORS_CHAN "ipc:///tmp/actuators"
 #define AIRSPEED_CHAN "ipc:///tmp/airspeed"
-#define AHRS_CHAN "ipc:///tmp/ahrs"
 #define LOG_CHAN "ipc:///tmp/logs"
 #define LISA_CHAN "ipc:///tmp/lisa"
 
