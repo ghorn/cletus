81e18a40d896f0c05809a6447cc0fb66dedba1d8
Fab <fab@ubuntu.(none)>
2014-08-16 13:38:50 +0200

    Adapted for ProtoBuf. Controller without function yet.

--- run_controller.c
+++ run_controller.c
@@ -19,22 +19,16 @@
 #include "./misc.h"
 
 #include "./controller.h"
+#include "./protos_c/messages.pb-c.h"
 
 
-#ifdef ALL
-#define IMU
-#define RC
-#define AHRS
-#define AIRSPEED
-#define GPS
-#endif
+
+#define MAX_MSG_SIZE 512
+
 
 /* ZMQ resources */
 static void *zctx = NULL;
-static void *zsock_imu = NULL;
-static void *zsock_gps = NULL;
-static void *zsock_ahrs = NULL;
-static void *zsock_airspeed = NULL;
+static void *zsock_sensors = NULL;
 static void *zsock_actuators = NULL;
 static void *zsock_log = NULL;
 
@@ -42,144 +36,118 @@ static void *zsock_log = NULL;
 int txfails = 0, rxfails = 0;
 
 static void __attribute__((noreturn)) die(int code) {
-  zdestroy(zsock_actuators, NULL);
-  zdestroy(zsock_log, NULL);
-  zdestroy(zsock_imu, zctx);
-  zdestroy(zsock_gps, zctx);
-  zdestroy(zsock_ahrs, zctx);
-  zdestroy(zsock_airspeed, zctx);
+    zdestroy(zsock_actuators, NULL);
+    zdestroy(zsock_log, NULL);
+    zdestroy(zsock_sensors, zctx);
 
 
 
-  printf("%d TX fails; %d RX fails.\n", txfails, rxfails);
-  printf("Moriturus te saluto!\n");
-  exit(code);
+    printf("%d TX fails; %d RX fails.\n", txfails, rxfails);
+    printf("Moriturus te saluto!\n");
+    exit(code);
 }
 
 /* Our signal to GTFO */
 static int bail = 0;
 
 static void sigdie(int signum) {
-  bail = signum;
+    bail = signum;
 }
 
 int main(int argc __attribute__((unused)),
          char **argv __attribute__((unused))) {
 
 
-  struct sched_param param;
-  set_priority(&param, RT_PRIORITY);
-  stack_prefault();
-  struct timespec t;
-  const int rt_interval = RT_INTERVAL;
+    struct sched_param param;
+    set_priority(&param, RT_PRIORITY);
+    stack_prefault();
+    struct timespec t;
+    const int rt_interval = RT_INTERVAL;
 
 
 
-  setbuf(stdin, NULL);
-  /* Confignals. */
-  if (signal(SIGINT, &sigdie) == SIG_IGN)
-    signal(SIGINT, SIG_IGN);
-  if (signal(SIGTERM, &sigdie) == SIG_IGN)
-    signal(SIGTERM, SIG_IGN);
-  if (signal(SIGHUP, &sigdie) == SIG_IGN)
-    signal(SIGHUP, SIG_IGN);
-  if (signal(SIGABRT, &sigdie) == SIG_IGN)
-    signal(SIGABRT, SIG_IGN);
+    setbuf(stdin, NULL);
+    /* Confignals. */
+    if (signal(SIGINT, &sigdie) == SIG_IGN)
+        signal(SIGINT, SIG_IGN);
+    if (signal(SIGTERM, &sigdie) == SIG_IGN)
+        signal(SIGTERM, SIG_IGN);
+    if (signal(SIGHUP, &sigdie) == SIG_IGN)
+        signal(SIGHUP, SIG_IGN);
+    if (signal(SIGABRT, &sigdie) == SIG_IGN)
+        signal(SIGABRT, SIG_IGN);
 
-  /* ZMQ setup first. */
+    /* ZMQ setup first. */
 
-  zsock_imu = setup_zmq_receiver(IMU_CHAN, &zctx, ZMQ_SUB, NULL, 1, 500);
-  if (NULL == zsock_imu)
-    return 1;
-  zsock_gps = setup_zmq_receiver(GPS_CHAN, &zctx, ZMQ_SUB, NULL, 1, 500);
-  if (NULL == zsock_gps)
-    return 1;
-  zsock_airspeed = setup_zmq_receiver(AIRSPEED_CHAN, &zctx, ZMQ_SUB, NULL, 1, 500);
-  if (NULL == zsock_airspeed)
-    return 1;
-  zsock_ahrs = setup_zmq_receiver(AHRS_CHAN, &zctx, ZMQ_SUB, NULL, 1, 500);
-  if (NULL == zsock_ahrs)
-    return 1;
-  zsock_actuators = setup_zmq_sender(ACTUATORS_CHAN, &zctx, ZMQ_PUB, 1, 500);
-  if (NULL == zsock_actuators)
-    die(1);
-  zsock_log = setup_zmq_sender(LOG_CHAN, &zctx, ZMQ_PUB, 1000, 100000);
-  if (NULL == zsock_log)
-    die(1);
+    zsock_sensors = setup_zmq_receiver(SENSORS_CHAN, &zctx, ZMQ_SUB, NULL, 1, 500);
+    if (NULL == zsock_sensors)
+        return 1;
+    zsock_actuators = setup_zmq_sender(ACTUATORS_CHAN, &zctx, ZMQ_PUB, 1, 500);
+    if (NULL == zsock_actuators)
+        die(1);
+    zsock_log = setup_zmq_sender(LOG_CHAN, &zctx, ZMQ_PUB, 1000, 100000);
+    if (NULL == zsock_log)
+        die(1);
 
-  /* Data storage. */
-//  sensors_t y_incoming;
-//  actuators_t u_outgoing;
+    /* Data storage. */
+    //  sensors_t y_incoming;
+    //  actuators_t u_outgoing;
 
 
-  zmq_pollitem_t polls[] = {
+    zmq_pollitem_t polls[] = {
 
-    {
-      .socket=zsock_imu,
-      .fd=-1,
-      .events= ZMQ_POLLIN,
-      .revents=0
-    },
-    {
-      .socket=zsock_gps,
-      .fd=-1,
-      .events= ZMQ_POLLIN,
-      .revents=0
-    },
-    {
-      .socket=zsock_ahrs,
-      .fd=-1,
-      .events= ZMQ_POLLIN,
-      .revents=0
-    },
-    {
-      .socket=zsock_airspeed,
-      .fd=-1,
-      .events= ZMQ_POLLIN,
-      .revents=0
-    },
-    {
-      /* Outputs (our socket to send data to the actuators and the
-                                               * logger socket) */
-      .socket = zsock_actuators,
-      .fd = -1,
-      /* 'events' would be ZMQ_POLLOUT, but we'll wait till we have
-                                                   * something to send*/
-      .events = 0,
-      .revents = 0
-    },
-    {
-      .socket = zsock_log,
-      .fd = -1,
-      .events = 0,
-      .revents = 0
-    }
-  };
+        {
+            .socket=zsock_sensors,
+            .fd=-1,
+            .events= ZMQ_POLLIN,
+            .revents=0
+        },
+        {
+            /* Outputs (our socket to send data to the actuators and the
+                                                       * logger socket) */
+            .socket = zsock_actuators,
+            .fd = -1,
+            /* 'events' would be ZMQ_POLLOUT, but we'll wait till we have
+                                                           * something to send*/
+            .events = 0,
+            .revents = 0
+        },
+        {
+            .socket = zsock_log,
+            .fd = -1,
+            .events = 0,
+            .revents = 0
+        }
+    };
 
-#ifdef IMU
-//  zmq_pollitem_t* poll_imu = &polls[0];
-#endif
-#ifdef GPS
-  zmq_pollitem_t* poll_gps = &polls[1];
-#endif
-#ifdef AHRS
-  zmq_pollitem_t* poll_ahrs = &polls[2];
-#endif
-#ifdef AIRSPEED
-  zmq_pollitem_t* poll_airspeed = &polls[3];
-#endif
-//  zmq_pollitem_t* poll_actuators = &polls[4];
-//  zmq_pollitem_t* poll_log = &polls[5];
+    //Pollitem for sensors
+    zmq_pollitem_t* poll_sensors = &polls[0];
+    //Pollitem for actuators
+    zmq_pollitem_t* poll_actuators = &polls[1];
+    //Pollitem flor logging
+    zmq_pollitem_t* poll_log = &polls[2];
+
+
+    uint8_t zmq_buffer[MAX_MSG_SIZE]; // Input data container for bytes
+
+    //Placeholders for PROTOBUF data
+    SensorsProto *sensors_ptr;         // Sensors
+    ActuatorsProto actuators = ACTUATORS_PROTO__INIT;
+    TimestampProto actuators_timstamp_start = TIMESTAMP_PROTO__INIT;
+    TimestampProto actuators_timstamp_stop = TIMESTAMP_PROTO__INIT;
+    actuators.start = &actuators_timstamp_start;
+    actuators.stop = &actuators_timstamp_stop;
 
 
 
-  const int npolls = sizeof(polls) / sizeof(polls[0]);
+    const u_int8_t npolls = sizeof(polls) / sizeof(polls[0]);
+    u_int32_t length;
 
-  clock_gettime(CLOCK_MONOTONIC ,&t);
-  /* start after one second */
-  t.tv_sec++;
+    clock_gettime(CLOCK_MONOTONIC ,&t);
+    /* start after one second */
+    t.tv_sec++;
 
-  /* Here's the main loop -- we only do stuff when input or output
+    /* Here's the main loop -- we only do stuff when input or output
        * happens.  The timeout can be put to good use, or you can also use
        * timerfd_create() to create a file descriptor with a timer under
        * the hood and dispatch on that.
@@ -188,83 +156,106 @@ int main(int argc __attribute__((unused)),
        * you'd want to pull most of the actual handling out into functions
        * and simply loop over your polls; I've left it all inline here
        * mostly out of laziness. */
-  for (;;) {
-      if (bail) die(bail);
+    for (;;) {
+        if (bail) die(bail);
 
-      /* wait until next shot */
-      clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t, NULL);
-      /* Poll for activity; time out after 10 milliseconds. */
-      const int polled = zmq_poll(polls, npolls, 10);
-      if (polled < 0) {
-          if (bail) die(bail);
-          zerr("while polling");
-          calc_next_shot(&t,rt_interval);
-          continue;
+        /* wait until next shot */
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t, NULL);
+        /* Poll for activity; time out after 10 milliseconds. */
+        const int polled = zmq_poll(polls, npolls, 10);
+        if (polled < 0) {
+            if (bail) die(bail);
+            zerr("while polling");
+            calc_next_shot(&t,rt_interval);
+            continue;
         } else if (polled == 0) {
-          if (bail) die(bail);
-          calc_next_shot(&t,rt_interval);
-          continue;
+            if (bail) die(bail);
+            calc_next_shot(&t,rt_interval);
+            continue;
         }
 
 
 
-//      if (bail) die(bail);
-//      if (poll_imu->revents & ZMQ_POLLIN) {
-//          const int zr = zmq_recvm(zsock_imu, (uint8_t *) &y_incoming.imu,
-//                                   sizeof(y_incoming.imu));
-//          if (zr < (int) sizeof(imu_t)) {
-//              err("couldn't read sensors!");
-//              rxfails++;
-//              /* Better clear the output flag, in case we corrupted the
-//                   * data. */
-//              poll_actuators->events = 0;
-//              poll_log->events = 0;
-//            } else {
-//#ifdef DEBUG
-//              printf("read from sensors OK!\n");
-//#endif
-//              /* Here is where you might run your controller when you get a
-//                   * complete set of sensor inputs. */
-//              run_demo_controller(&y_incoming, &u_outgoing);
-//              /* Controller went OK (it had damn well better) -- enable
-//                   * output sockets. */
-//#ifdef DEBUG
-//              printf("New Control values: Rudder -> %f \n\t Flaps -> %f\n\t Ailerons -> %f\n\t Elevator -> %f\n",
-//                     u_outgoing.rudd, u_outgoing.flaps, u_outgoing.ail, u_outgoing.elev);
-//#endif
-//              poll_actuators->events = ZMQ_POLLOUT;
-//              poll_log->events = ZMQ_POLLOUT;
-//            }
-//          /* Clear the poll state. */
-//          poll_imu->revents = 0;
-//        }
+        if (bail) die(bail);
+        if (poll_sensors->revents & ZMQ_POLLIN) {
+            const int zr = zmq_recvm(zsock_sensors, zmq_buffer,
+                                     MAX_MSG_SIZE);
+            if (zr < (int) MAX_MSG_SIZE)
+            {
+                err("couldn't read sensors!");
+                rxfails++;
+                /* Better clear the output flag, in case we corrupted the
+                   * data. */
+                poll_actuators->events = 0;
+                poll_log->events = 0;
+            }
+            else
+            {
+                sensors_ptr = sensors_proto__unpack(NULL, MAX_MSG_SIZE, zmq_buffer);
+#ifdef DEBUG
+                printf("Controller received Sensor data containing: ");
+                switch (sensors_ptr->type) {
+                case SENSORS_PROTO__TYPE__IMU_ONLY:
+                    printf("IMU_ONLY\n");
+                    break;
+                case SENSORS_PROTO__TYPE__IMU_GPS:
+                    printf("IMU ; GPS\n");
+                    break;
+                case SENSORS_PROTO__TYPE__IMU_AIRSPEED:
+                    printf("IMU ; AIRSPEED\n");
+                    break;
+                case SENSORS_PROTO__TYPE__IMU_GPS_AIRSPEED:
+                    printf("IMU ; GPS ; AIRSPEED\n");
+                    break;
+                case _SENSORS_PROTO__TYPE_IS_INT_SIZE:
+                    printf("INT SIZE case -> no vailid type\n");
+                    break;
+                default:
+                    printf("UNKNOWN TYPE\n");
+                    break;
+                }
+#endif
+                /* Here is where you might run your controller when you get a
+                   * complete set of sensor inputs. */
+                //run_demo_controller(&y_incoming, &u_outgoing);
+                /* Controller went OK (it had damn well better) -- enable
+                   * output sockets. */
+#ifdef DEBUG
+                printf("New Control values: Rudder -> %f \n\t Flaps -> %f\n\t Ailerons -> %f\n\t Elevator -> %f\n",
+                       actuators.rudd, actuators.flaps, actuators.ail, actuators.elev);
+#endif
+                poll_actuators->events = ZMQ_POLLOUT;
+                poll_log->events = ZMQ_POLLOUT;
+            }
+            /* Clear the poll state. */
+            poll_sensors->revents = 0;
+        }
 
-//      if (bail) die(bail);
-//      if (poll_actuators->revents & ZMQ_POLLOUT) {
-//          const void *bufs[] = {&u_outgoing};
-//          const uint32_t lens[] = {sizeof(u_outgoing)};
-//          const int zs = zmq_sendm(zsock_actuators, bufs, lens,
-//                                   sizeof(lens) / sizeof(lens[0]));
-//          if (zs < 0) {
-//              txfails++;
-//            } else {
-//#ifdef DEBUG
-//              printf("Sent to actuators!\n");
-//#endif
-//              /* Clear the events flag so we won't try to send until we
-//                   * have more data. */
-//              poll_actuators->events = 0;
-//              /* calculate next shot */
+        if (bail) die(bail);
+        if (poll_actuators->revents & ZMQ_POLLOUT) {
+            length = actuators_proto__get_packed_size(&actuators); //
+            actuators_proto__pack(&actuators, zmq_buffer);
+            const int zs = zmq_sendm(zsock_actuators,(void*) zmq_buffer, &length,1);
+            if (zs < 0) {
+                txfails++;
+            } else {
+#ifdef DEBUG
+                printf("Sent to actuators!\n");
+#endif
+                /* Clear the events flag so we won't try to send until we
+                   * have more data. */
+                poll_actuators->events = 0;
+                /* calculate next shot */
 
-//            }
-//          poll_actuators->revents = 0;
-//        }
-      calc_next_shot(&t,rt_interval);
+            }
+            poll_actuators->revents = 0;
+        }
+        calc_next_shot(&t,rt_interval);
 
 
-      /* I skipped logging; I think you know what to do. */
+        /* I skipped logging; I think you know what to do. */
     }
 
-  /* Shouldn't get here. */
-  return 0;
+    /* Shouldn't get here. */
+    return 0;
 }
