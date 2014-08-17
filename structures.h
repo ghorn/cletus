79cd3298e7bbb181e8638da6e64c87b7fc48a933
Fab <fab@ubuntu.(none)>
2014-08-17 18:22:52 +0200

    Added id to match received data.

--- structures.h
+++ structures.h
@@ -11,168 +11,175 @@
 
 //DATATYPES
 typedef struct __attribute__((packed)){
-  uint64_t tsec;
-  uint64_t tnsec;
+    uint64_t tsec;
+    uint64_t tnsec;
 } timestamp_t;
 
 typedef struct __attribute__((packed)){
-  double x;
-  double y;
-  double z;
+    double x;
+    double y;
+    double z;
 } xyz_double;
 
 typedef struct __attribute__((packed)){
-  int x;
-  int y;
-  int z;
+    int32_t x;
+    int32_t y;
+    int32_t z;
 } xyz_int;
 
 typedef struct __attribute__((packed)){
-  int32_t qi;
-  int32_t qx;
-  int32_t qy;
-  int32_t qz;
+    int32_t qi;
+    int32_t qx;
+    int32_t qy;
+    int32_t qz;
 }quaternion_t;
 
 typedef struct __attribute__((packed)){
-  double qi;
-  double qx;
-  double qy;
-  double qz;
+    double qi;
+    double qx;
+    double qy;
+    double qz;
 }quaternion_double_t;
 
 
 typedef struct __attribute__((packed)){
-  uint8_t message_length;
-  uint8_t sender_id;
-  uint8_t message_id;
+    uint8_t message_length;
+    uint8_t sender_id;
+    uint8_t message_id;
 } message_header_t;
 
 typedef struct __attribute__((packed)){
-  uint8_t checksum1;
-  uint8_t checksum2;
+    uint8_t checksum1;
+    uint8_t checksum2;
 } message_footer_t;
 
 //SENSORS STRUCTS
 typedef struct __attribute__((packed))
 {
-  xyz_int data;
-  timestamp_t timestamp;
+    uint8_t id;
+    xyz_int data;
+    timestamp_t timestamp;
 }gyro_raw_t;
 
 typedef struct __attribute__((packed))
 {
-  xyz_int data;
-  timestamp_t timestamp;
+    uint8_t id;
+    xyz_int data;
+    timestamp_t timestamp;
 }mag_raw_t;
 
 typedef struct __attribute__((packed))
 {
-  xyz_int data;
-  timestamp_t timestamp;
+    uint8_t id;
+    xyz_int data;
+    timestamp_t timestamp;
 }accel_raw_t;
 
 typedef struct __attribute__((packed))
 {
-  xyz_double data;
-  timestamp_t timestamp;
+    uint8_t id;
+    xyz_double data;
+    timestamp_t timestamp;
 }gyro_scaled_t;
 
 typedef struct __attribute__((packed))
 {
-  xyz_double data;
-  timestamp_t timestamp;
+    uint8_t id;
+    xyz_double data;
+    timestamp_t timestamp;
 }mag_scaled_t;
 
 typedef struct __attribute__((packed))
 {
-  xyz_double data;
-  timestamp_t timestamp;
+    uint8_t id;
+    xyz_double data;
+    timestamp_t timestamp;
 }accel_scaled_t;
 
 typedef struct __attribute__((packed))
 {
-  xyz_int pos_data;
-  xyz_int vel_data;
-  timestamp_t timestamp;
+    uint8_t id;
+    xyz_int pos_data;
+    xyz_int vel_data;
+    timestamp_t timestamp;
 }gps_t;
 
 
 typedef struct __attribute__((packed))
 {
-  accel_scaled_t imu_accel_scaled;
-  gyro_scaled_t imu_gyro_scaled;
-  mag_scaled_t imu_mag_scaled;
+    accel_scaled_t imu_accel_scaled;
+    gyro_scaled_t imu_gyro_scaled;
+    mag_scaled_t imu_mag_scaled;
 }imu_scaled_t;
 
 typedef struct __attribute__((packed))
 {
-  accel_raw_t imu_accel;
-  gyro_raw_t imu_gyro;
-  mag_raw_t imu_mag;
+    accel_raw_t imu_accel;
+    gyro_raw_t imu_gyro;
+    mag_raw_t imu_mag;
 }imu_raw_t;
 
 typedef struct __attribute__((packed)){
-  quaternion_t imu;
-  quaternion_t body;
-  timestamp_t timestamp;
+    quaternion_t imu;
+    quaternion_t body;
+    timestamp_t timestamp;
 }ahrs_int_t;
 
 typedef struct __attribute__((packed)){
-  quaternion_double_t imu_converted;
-  quaternion_double_t body_converted;
-  timestamp_t timestamp;
+    quaternion_double_t imu_converted;
+    quaternion_double_t body_converted;
+    timestamp_t timestamp;
 }ahrs_double_t;
 
 
 
 
 typedef struct __attribute__((packed)){
-  int8_t roll;
-  int8_t pitch;
-  int8_t yaw;
-  int8_t mode;
-  int8_t kill;
-  uint8_t status;
+    int8_t roll;
+    int8_t pitch;
+    int8_t yaw;
+    int8_t mode;
+    int8_t kill;
+    uint8_t status;
 } rc_t;
 
 typedef struct __attribute__((packed)) {
-  imu_scaled_t imu_scaled;
-  imu_raw_t imu_raw;
-  gps_t gps;
-  ahrs_int_t ahrs_int;
-  ahrs_double_t ahrs_double;
-  rc_t rc;
+    imu_scaled_t imu_scaled;
+    imu_raw_t imu_raw;
+    gps_t gps;
+    ahrs_int_t ahrs_int;
+    ahrs_double_t ahrs_double;
+    rc_t rc;
 } lisa_messages_t;
 
 typedef struct __attribute__((packed)) {
-  timestamp_t start;
-  timestamp_t stop;
-  double flaps;
-  double ail;
-  double rudd;
-  double elev;
+    timestamp_t start;
+    timestamp_t stop;
+    double flaps;
+    double ail;
+    double rudd;
+    double elev;
 } actuators_t;
 
 typedef struct __attribute__((packed)) {
-  int16_t servo_1;
-  int16_t servo_2;
-  int16_t servo_3;
-  int16_t servo_4;
-  int16_t servo_5;
-  int16_t servo_6;
-  int16_t servo_7;
+    int16_t servo_1;
+    int16_t servo_2;
+    int16_t servo_3;
+    int16_t servo_4;
+    int16_t servo_5;
+    int16_t servo_6;
+    int16_t servo_7;
 } servo_message_t;
 
 typedef struct __attribute__((packed)){
-  uint8_t startbyte;
-  uint8_t length;
-  uint8_t sender_id;
-  uint8_t message_id;
-  servo_message_t servos_msg;
-  uint8_t checksum1;
-  uint8_t checksum2;
-  }lisa_message_t;
+    uint8_t startbyte;
+    uint8_t length;
+    uint8_t sender_id;
+    uint8_t message_id;
+    servo_message_t servos_msg;
+    uint8_t checksum1;
+    uint8_t checksum2;
+}lisa_message_t;
 
 #define LOG_MESSAGE_SENSORS 0
 #define LOG_MESSAGE_ESTIMATOR 1
